5e7d9f7a33c8af844aad325760ccc759a499c189
Alexander Allan <45302@student.longroad.ac.uk>
2014-10-14 15:15:55 +0100

    Committed

---
+++ Iteration Exercise 1(Starter).py
@@ -0,0 +1,18 @@
+#Alexander Allan
+#14/10/2014
+#Iteration exercise 1
+
+##program to prompt for 8 numbers and report the total to the user
+
+Number1 = int(input("Enter number 1 : " ))
+Number2 = int(input("Enter number 2 : " ))
+Number3 = int(input("Enter number 3 : " ))
+Number4 = int(input("Enter number 4 : " ))
+Number5 = int(input("Enter number 5 : " ))
+Number6 = int(input("Enter number 6 : " ))
+Number7 = int(input("Enter number 7 : " ))
+Number8 = int(input("Enter number 8 : " ))
+
+Total = Number1 + Number2 + Number3 + Number4 + Number5 + Number6 + Number7 + Number8
+print("The total is {0}".format(Total))
+
